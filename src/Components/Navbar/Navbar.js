8687dea09da445285416bc3aa1fee709e13243f4
Haitam Jebari <hjebari@digital-place.co>
2023-05-22 09:15:01 +0200

    New Update 2023

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,11 +6,7 @@ function Navbar() {
   return (
     <>
       <div className='navbar'>
-<<<<<<< HEAD
-        <img src={logo} className='logonav'/>
-=======
         <img className='logonav' src={logo}/>   
->>>>>>> a77000da25d78dbc28faf5823896ce8dac458284
       </div>
     </>
   );
